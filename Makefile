f33f400d00ba79c7ac41133737672bbfdecfddc3
=devansh-bolsure <devansh-bolsure@users.noreply.github.com>
2025-04-25 23:48:26 +0530

    adding the project for further process

---
+++ Makefile
@@ -0,0 +1,22 @@
+install:
+	pip install --upgrade pip &&\
+	pip install -r "C:\\Users\\DEVANSH BOLSURE\\Dropbox\\My PC (LAPTOP-9PET8DJR)\\Documents\\anime\\MOVIES\\python_practice\\for_job\\ci_cd_project\\requirements.txt"
+
+format:
+	black *.py
+
+train:
+	python train.py
+
+eval:
+	echo "## Model Metrics" > report.md 
+	cat "C:\\Users\\DEVANSH BOLSURE\\Dropbox\\My PC (LAPTOP-9PET8DJR)\\Documents\\anime\\MOVIES\\python_practice\\for_job\\ci_cd_project\\Result\\metrics.txt" >> report.md
+
+	echo '\n## Confusion Matrix Plot' >> report.md
+	echo '![Confusion Matrix](C:\\Users\\DEVANSH BOLSURE\\Dropbox\\My PC (LAPTOP-9PET8DJR)\\Documents\\anime\\MOVIES\\python_practice\\for_job\\ci_cd_project\\Result\\model_results.png)' >> report.md 
+
+	cml comment create report.md
+
+
+git commit -am 'new_changes'
+git push origin main
---
+++ train.py
@@ -0,0 +1,92 @@
+
+#Importing libraries  and Loading the dataset
+import pandas as pd
+
+drug_df=pd.read_csv(r'C:\Users\DEVANSH BOLSURE\Dropbox\My PC (LAPTOP-9PET8DJR)\Documents\anime\MOVIES\python_practice\for_job\ci_cd_project\Data\drug200.csv')
+drud_df=drug_df.sample(frac=1)
+drug_df.head(3)
+
+from sklearn.model_selection import train_test_split
+x=drug_df.drop('Drug',axis=1)
+y=drug_df['Drug']
+
+print(x.shape,y.shape)
+
+"""Train Test Split"""
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=125)
+
+print(x_train.shape,y_train.shape,x_test.shape,y_test.shape)
+
+"""Machine Learning Pipeline"""
+
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+
+cat_col=[1,2,3]
+num_col=[0,4]
+
+transform=ColumnTransformer(
+    [
+        ('encoder',OrdinalEncoder(),cat_col),
+        ('num_imputer',SimpleImputer(strategy='median'),num_col),
+        ('num_scaler',StandardScaler(),num_col)
+    ]
+)
+
+pipe=Pipeline(
+    steps=[
+        ('preprocessing',transform),
+        ('model',RandomForestClassifier(n_estimators=100,random_state=150))
+    ]
+)
+
+pipe.fit(x_train,y_train)
+
+"""Model Evaluation"""
+
+from sklearn.metrics import classification_report
+y_pred=pipe.predict(x_test)
+report=classification_report(y_test,y_pred)
+
+print(report)
+
+with open(r'C:\Users\DEVANSH BOLSURE\Dropbox\My PC (LAPTOP-9PET8DJR)\Documents\anime\MOVIES\python_practice\for_job\ci_cd_project\Result\metrics.txt','w') as outfile:
+    outfile.write(f'\nclassification_report = {report}')
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay,confusion_matrix
+cm=confusion_matrix(y_test,y_pred,labels=pipe.classes_)
+disp=ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=pipe.classes_)
+disp.plot()
+plt.savefig(r'C:\Users\DEVANSH BOLSURE\Dropbox\My PC (LAPTOP-9PET8DJR)\Documents\anime\MOVIES\python_practice\for_job\ci_cd_project\Result\model_results.png',dpi=120)
+
+"""Saving the Model"""
+
+import skops.io as sio
+
+sio.dump(pipe,r'C:\Users\DEVANSH BOLSURE\Dropbox\My PC (LAPTOP-9PET8DJR)\Documents\anime\MOVIES\python_practice\for_job\ci_cd_project\Model\drug_pipeline.skops')
+
+from skops import io as sio
+
+file_path = r'C:\Users\DEVANSH BOLSURE\Dropbox\My PC (LAPTOP-9PET8DJR)\Documents\anime\MOVIES\python_practice\for_job\ci_cd_project\Model\drug_pipeline.skops'
+
+# Open the file in binary mode
+with open(file_path, "rb") as f:
+    untrusted = sio.get_untrusted_types(file=f)
+
+
+print(untrusted)
+
+import numpy as np
+
+trusted = ['np.dtype']
+
+with open(file_path,'rb') as f:
+    model = sio.load(file=f, trusted=untrusted)
+
+print(model)
+
